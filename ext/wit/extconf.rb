18e4c390f9720e1dde8d8306e2c0d96eb76db21e
patapizza <julien@wit.ai>
2014-10-03 15:01:20 -0700

    adding new linux64 lib

--- ext/wit/extconf.rb
+++ ext/wit/extconf.rb
@@ -14,21 +14,23 @@ LIB_DIRS = [
 ]
 
 if RUBY_PLATFORM.include? 'arm'
-	system('ln -s libwit-armv6.a libwit.a', :chdir=> PATH + '/libwit/lib')
+	system('curl -o libwit.a https://raw.githubusercontent.com/wit-ai/libwit-ruby/master/ext/wit/libwit/lib/libwit-armv6.a', :chdir=> PATH + '/libwit/lib')
 elsif RUBY_PLATFORM.include? '64'
 	if RUBY_PLATFORM.include? 'darwin'
-		system('ln -s libwit-64-darwin.a libwit.a', :chdir=> PATH + '/libwit/lib')
+		system('curl -o libwit.a https://raw.githubusercontent.com/wit-ai/libwit-ruby/master/ext/wit/libwit/lib/libwit-64-darwin.a', :chdir=> PATH + '/libwit/lib')
 	else
-		system('ln -s libwit-64-linux.a libwit.a', :chdir=> PATH + '/libwit/lib')
+		system('curl -o libwit.a https://raw.githubusercontent.com/wit-ai/libwit-ruby/master/ext/wit/libwit/lib/libwit-64-linux.a', :chdir=> PATH + '/libwit/lib')
 	end
 else
-	system('ln -s libwit-32-linux.a libwit.a', :chdir=> PATH + '/libwit/lib')
+	system('curl -o libwit.a https://raw.githubusercontent.com/wit-ai/libwit-ruby/master/ext/wit/libwit/lib/libwit-32-linux.a', :chdir=> PATH + '/libwit/lib')
 end
 
 $LOCAL_LIBS = '-lwit -lsox -lcurl'
 
 dir_config 'wit', HEADER_DIRS, LIB_DIRS
 
+abort "missing sox" unless have_library "sox"
+abort "missing curl" unless have_library "curl"
 abort "missing wit.h" unless have_header "wit.h"
 abort "missing libwit" unless have_library "wit"
 
