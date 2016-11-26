9824de71b249b1cbbcd6f7ee067651098e96b11f
Ben Klang <bklang@mojolingo.com>
2016-11-26 14:35:35 -0500

    Idiomatic Ruby Cleanups (#14)
    * Move Wit exception wrapper inside namespace
    
    Also, inherit from StandardError instead of Exception. See http://stackoverflow.com/questions/10048173/why-is-it-bad-style-to-rescue-exception-e-in-ruby#10048406
    
    * Move Wit constants inside Wit namespace
    
    * Move global methods to private methods within Wit
    
    Libraries should not define global methods
    
    * Move #__run_actions within private code area
    
    * Version bump for Exception class name change

--- lib/wit.rb
+++ lib/wit.rb
@@ -3,64 +3,14 @@ require 'logger'
 require 'net/http'
 require 'securerandom'
 
-WIT_API_HOST = ENV['WIT_URL'] || 'https://api.wit.ai'
-WIT_API_VERSION = ENV['WIT_API_VERSION']  || '20160516'
-DEFAULT_MAX_STEPS = 5
-LEARN_MORE = 'Learn more at https://wit.ai/docs/quickstart'
-
-class WitException < Exception
-end
-
-def req(logger, access_token, meth_class, path, params={}, payload={})
-  uri = URI(WIT_API_HOST + path)
-  uri.query = URI.encode_www_form(params)
-
-  logger.debug("#{meth_class} #{uri}")
-
-  request = meth_class.new(uri)
-  request['authorization'] = 'Bearer ' + access_token
-  request['accept'] = 'application/vnd.wit.' + WIT_API_VERSION + '+json'
-  request.add_field 'Content-Type', 'application/json'
-  request.body = payload.to_json
-
-  Net::HTTP.start(uri.host, uri.port, {:use_ssl => uri.scheme == 'https'}) do |http|
-    rsp = http.request(request)
-    if rsp.code.to_i != 200
-      raise WitException.new("HTTP error code=#{rsp.code}")
-    end
-    json = JSON.parse(rsp.body)
-    if json.has_key?('error')
-      raise WitException.new("Wit responded with an error: #{json['error']}")
-    end
-    logger.debug("#{meth_class} #{uri} #{json}")
-    json
-  end
-end
-
-def validate_actions(logger, actions)
-  [:send].each do |action|
-    if !actions.has_key?(action)
-      logger.warn "The #{action} action is missing. #{LEARN_MORE}"
-    end
-  end
-  actions.each_pair do |k, v|
-    if !k.is_a?(Symbol)
-      logger.warn "The '#{k}' action name should be a symbol"
-    end
-    if !(v.respond_to?(:call) && v.lambda?)
-      logger.warn "The '#{k}' action should be a lambda function"
-    end
-    if k == :send && v.arity != 2
-      logger.warn "The \'send\' action should take 2 arguments: request and response. #{LEARN_MORE}"
-    end
-    if k != :send && v.arity != 1
-      logger.warn "The '#{k}' action should take 1 argument: request. #{LEARN_MORE}"
-    end
-  end
-  return actions
-end
-
 class Wit
+  class Error < StandardError; end
+
+  WIT_API_HOST = ENV['WIT_URL'] || 'https://api.wit.ai'
+  WIT_API_VERSION = ENV['WIT_API_VERSION']  || '20160516'
+  DEFAULT_MAX_STEPS = 5
+  LEARN_MORE = 'Learn more at https://wit.ai/docs/quickstart'
+
   def initialize(opts = {})
     @access_token = opts[:access_token]
 
@@ -92,7 +42,7 @@ class Wit
 
   def converse(session_id, msg, context={}, reset=nil)
     if !context.is_a?(Hash)
-      raise WitException.new('context should be a Hash')
+      raise Error.new('context should be a Hash')
     end
     params = {}
     params[:q] = msg unless msg.nil?
@@ -102,73 +52,12 @@ class Wit
     return res
   end
 
-  def __run_actions(session_id, current_request, message, context, i)
-    if i <= 0
-      raise WitException.new('Max steps reached, stopping.')
-    end
-    json = converse(session_id, message, context)
-    if json['type'].nil?
-      raise WitException.new('Couldn\'t find type in Wit response')
-    end
-    if current_request != @_sessions[session_id]
-      return context
-    end
-
-    logger.debug("Context: #{context}")
-    logger.debug("Response type: #{json['type']}")
-
-    # backwards-compatibility with API version 20160516
-    if json['type'] == 'merge'
-      json['type'] = 'action'
-      json['action'] = 'merge'
-    end
-
-    if json['type'] == 'error'
-      raise WitException.new('Oops, I don\'t know what to do.')
-    end
-
-    if json['type'] == 'stop'
-      return context
-    end
-
-    request = {
-      'session_id' => session_id,
-      'context' => context.clone,
-      'text' => message,
-      'entities' => json['entities']
-    }
-    if json['type'] == 'msg'
-      throw_if_action_missing(:send)
-      response = {
-        'text' => json['msg'],
-        'quickreplies' => json['quickreplies'],
-      }
-      @actions[:send].call(request, response)
-    elsif json['type'] == 'action'
-      action = json['action'].to_sym
-      throw_if_action_missing(action)
-      context = @actions[action].call(request)
-      if context.nil?
-        logger.warn('missing context - did you forget to return it?')
-        context = {}
-      end
-    else
-      raise WitException.new("unknown type: #{json['type']}")
-    end
-
-    if current_request != @_sessions[session_id]
-      return context
-    end
-
-    return __run_actions(session_id, current_request, nil, context, i - 1)
-  end
-
   def run_actions(session_id, message, context={}, max_steps=DEFAULT_MAX_STEPS)
     if !@actions
       throw_must_have_actions
     end
     if !context.is_a?(Hash)
-      raise WitException.new('context should be a Hash')
+      raise Error.new('context should be a Hash')
     end
 
     # Figuring out whether we need to reset the last turn.
@@ -206,7 +95,7 @@ class Wit
 
       begin
         context = run_actions(session_id, msg, context, max_steps)
-      rescue WitException => exp
+      rescue Error => exp
         logger.error("error: #{exp.message}")
       end
     end
@@ -216,13 +105,123 @@ class Wit
 
   def throw_if_action_missing(action_name)
     if !@actions.has_key?(action_name)
-      raise WitException.new("unknown action: #{action_name}")
+      raise Error.new("unknown action: #{action_name}")
     end
   end
 
   def throw_must_have_actions()
-    raise WitException.new('You must provide the `actions` parameter to be able to use runActions. ' + LEARN_MORE)
+    raise Error.new('You must provide the `actions` parameter to be able to use runActions. ' + LEARN_MORE)
   end
 
-  private :__run_actions
+  private
+
+  def __run_actions(session_id, current_request, message, context, i)
+    if i <= 0
+      raise Error.new('Max steps reached, stopping.')
+    end
+    json = converse(session_id, message, context)
+    if json['type'].nil?
+      raise Error.new('Couldn\'t find type in Wit response')
+    end
+    if current_request != @_sessions[session_id]
+      return context
+    end
+
+    logger.debug("Context: #{context}")
+    logger.debug("Response type: #{json['type']}")
+
+    # backwards-compatibility with API version 20160516
+    if json['type'] == 'merge'
+      json['type'] = 'action'
+      json['action'] = 'merge'
+    end
+
+    if json['type'] == 'error'
+      raise Error.new('Oops, I don\'t know what to do.')
+    end
+
+    if json['type'] == 'stop'
+      return context
+    end
+
+    request = {
+      'session_id' => session_id,
+      'context' => context.clone,
+      'text' => message,
+      'entities' => json['entities']
+    }
+    if json['type'] == 'msg'
+      throw_if_action_missing(:send)
+      response = {
+        'text' => json['msg'],
+        'quickreplies' => json['quickreplies'],
+      }
+      @actions[:send].call(request, response)
+    elsif json['type'] == 'action'
+      action = json['action'].to_sym
+      throw_if_action_missing(action)
+      context = @actions[action].call(request)
+      if context.nil?
+        logger.warn('missing context - did you forget to return it?')
+        context = {}
+      end
+    else
+      raise Error.new("unknown type: #{json['type']}")
+    end
+
+    if current_request != @_sessions[session_id]
+      return context
+    end
+
+    return __run_actions(session_id, current_request, nil, context, i - 1)
+  end
+
+  def req(logger, access_token, meth_class, path, params={}, payload={})
+    uri = URI(WIT_API_HOST + path)
+    uri.query = URI.encode_www_form(params)
+
+    logger.debug("#{meth_class} #{uri}")
+
+    request = meth_class.new(uri)
+    request['authorization'] = 'Bearer ' + access_token
+    request['accept'] = 'application/vnd.wit.' + WIT_API_VERSION + '+json'
+    request.add_field 'Content-Type', 'application/json'
+    request.body = payload.to_json
+
+    Net::HTTP.start(uri.host, uri.port, {:use_ssl => uri.scheme == 'https'}) do |http|
+      rsp = http.request(request)
+      if rsp.code.to_i != 200
+        raise Error.new("HTTP error code=#{rsp.code}")
+      end
+      json = JSON.parse(rsp.body)
+      if json.has_key?('error')
+        raise Error.new("Wit responded with an error: #{json['error']}")
+      end
+      logger.debug("#{meth_class} #{uri} #{json}")
+      json
+    end
+  end
+
+  def validate_actions(logger, actions)
+    [:send].each do |action|
+      if !actions.has_key?(action)
+        logger.warn "The #{action} action is missing. #{LEARN_MORE}"
+      end
+    end
+    actions.each_pair do |k, v|
+      if !k.is_a?(Symbol)
+        logger.warn "The '#{k}' action name should be a symbol"
+      end
+      if !(v.respond_to?(:call) && v.lambda?)
+        logger.warn "The '#{k}' action should be a lambda function"
+      end
+      if k == :send && v.arity != 2
+        logger.warn "The \'send\' action should take 2 arguments: request and response. #{LEARN_MORE}"
+      end
+      if k != :send && v.arity != 1
+        logger.warn "The '#{k}' action should take 1 argument: request. #{LEARN_MORE}"
+      end
+    end
+    return actions
+  end
 end
