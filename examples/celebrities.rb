8072c79deb87d845e6a2d426b93acce6394912bc
Aleka Cheung <leka@fb.com>
2018-02-07 15:45:06 -0800

    sunsetting stories: removing converse and run_actions

---
+++ examples/celebrities.rb
@@ -0,0 +1,48 @@
+require 'wit'
+
+if ARGV.length == 0
+  puts("usage: #{$0} <wit-access-token>")
+  exit 1
+end
+
+access_token = ARGV[0]
+ARGV.shift
+
+# Celebrities example
+# See https://wit.ai/aforaleka/wit-example-celebrities/
+
+def first_entity_value(entities, entity)
+  return nil unless entities.has_key? entity
+  val = entities[entity][0]['value']
+  return nil if val.nil?
+  return val
+end
+
+def handle_message(response)
+  entities = response['entities']
+  greetings = first_entity_value(entities, 'greetings')
+  celebrity = first_entity_value(entities, 'notable_person')
+
+  case
+  when celebrity
+    return wikidata_description(celebrity)
+  when greetings
+    return "Hi! You can say something like 'Tell me about Beyonce'"
+  else
+    return "Um. I don't recognize that name. " \
+            "Which celebrity do you want to learn about?"
+  end
+end
+
+def wikidata_description(celebrity)
+  return "I recognize " + celebrity['name'] unless celebrity.dig('external', 'wikidata')
+  wikidata_id = celebrity.fetch('external').fetch('wikidata')
+  api = URI("https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=#{wikidata_id}&props=descriptions&languages=en")
+  rsp = Net::HTTP.get_response(api)
+  wikidata = JSON.parse(rsp.body)
+  description = wikidata['entities'][wikidata_id]['descriptions']['en']['value']
+  return "ooo yes I know " + celebrity['name'] + " -- " + description
+end
+
+client = Wit.new(access_token: access_token)
+client.interactive(method(:handle_message))
--- lib/wit.rb
+++ lib/wit.rb
@@ -17,13 +17,6 @@ class Wit
     if opts[:logger]
       @logger = opts[:logger]
     end
-
-    if opts[:actions]
-      logger.warn('Stories and POST /converse have been deprecated. This will break in February 2018!')
-      @actions = validate_actions(logger, opts[:actions])
-    end
-
-    @_sessions = {}
   end
 
   def logger
@@ -43,47 +36,7 @@ class Wit
     return res
   end
 
-  def converse(session_id, msg, context={}, reset=nil)
-    if !context.is_a?(Hash)
-      raise Error.new('context should be a Hash')
-    end
-    params = {}
-    params[:q] = msg unless msg.nil?
-    params[:session_id] = session_id
-    params[:reset] = true if reset
-    res = req(logger, @access_token, Net::HTTP::Post, '/converse', params, context)
-    return res
-  end
-
-  def run_actions(session_id, message, context={}, max_steps=DEFAULT_MAX_STEPS)
-    if !@actions
-      throw_must_have_actions
-    end
-    if !context.is_a?(Hash)
-      raise Error.new('context should be a Hash')
-    end
-
-    # Figuring out whether we need to reset the last turn.
-    # Each new call increments an index for the session.
-    # We only care about the last call to run_actions.
-    # All the previous ones are discarded (preemptive exit).
-    current_request = 1
-    if @_sessions.has_key?(session_id)
-      current_request = @_sessions[session_id] + 1
-    end
-    @_sessions[session_id] = current_request
-
-    context = __run_actions(session_id, current_request, message, context, max_steps)
-
-    # Cleaning up once the last call to run_actions finishes.
-    if current_request == @_sessions[session_id]
-      @_sessions.delete(session_id)
-    end
-
-    return context
-  end
-
-  def interactive(context={})
+  def interactive(handle_message=nil, context={})
     while true
       print '> '
       msg = STDIN.gets.strip
@@ -92,23 +45,17 @@ class Wit
       end
 
       begin
-        puts(message(msg))
+        if handle_message.nil?
+          puts(message(msg))
+        else
+          puts(handle_message.call(message(msg)))
+        end
       rescue Error => exp
         logger.error("error: #{exp.message}")
       end
     end
   rescue Interrupt => _exp
     puts
-  end
-
-  def throw_if_action_missing(action_name)
-    if !@actions.has_key?(action_name)
-      raise Error.new("unknown action: #{action_name}")
-    end
-  end
-
-  def throw_must_have_actions()
-    raise Error.new('You must provide the `actions` parameter to be able to use runActions. ' + LEARN_MORE)
   end
 
   def get_entities
@@ -173,67 +120,6 @@ class Wit
     end
   end
 
-  def __run_actions(session_id, current_request, message, context, i)
-    if i <= 0
-      raise Error.new('Max steps reached, stopping.')
-    end
-    json = converse(session_id, message, context)
-    if json['type'].nil?
-      raise Error.new('Couldn\'t find type in Wit response')
-    end
-    if current_request != @_sessions[session_id]
-      return context
-    end
-
-    logger.debug("Context: #{context}")
-    logger.debug("Response type: #{json['type']}")
-
-    # backwards-compatibility with API version 20160516
-    if json['type'] == 'merge'
-      json['type'] = 'action'
-      json['action'] = 'merge'
-    end
-
-    if json['type'] == 'error'
-      raise Error.new('Oops, I don\'t know what to do.')
-    end
-
-    if json['type'] == 'stop'
-      return context
-    end
-
-    request = {
-      'session_id' => session_id,
-      'context' => context.clone,
-      'text' => message,
-      'entities' => json['entities']
-    }
-    if json['type'] == 'msg'
-      throw_if_action_missing(:send)
-      response = {
-        'text' => json['msg'],
-        'quickreplies' => json['quickreplies'],
-      }
-      @actions[:send].call(request, response)
-    elsif json['type'] == 'action'
-      action = json['action'].to_sym
-      throw_if_action_missing(action)
-      context = @actions[action].call(request)
-      if context.nil?
-        logger.warn('missing context - did you forget to return it?')
-        context = {}
-      end
-    else
-      raise Error.new("unknown type: #{json['type']}")
-    end
-
-    if current_request != @_sessions[session_id]
-      return context
-    end
-
-    return __run_actions(session_id, current_request, nil, context, i - 1)
-  end
-
   def req(logger, access_token, meth_class, path, params={}, payload={})
     uri = URI(WIT_API_HOST + path)
     uri.query = URI.encode_www_form(params)
@@ -258,28 +144,5 @@ class Wit
       logger.debug("#{meth_class} #{uri} #{json}")
       json
     end
-  end
-
-  def validate_actions(logger, actions)
-    [:send].each do |action|
-      if !actions.has_key?(action)
-        logger.warn "The #{action} action is missing. #{LEARN_MORE}"
-      end
-    end
-    actions.each_pair do |k, v|
-      if !k.is_a?(Symbol)
-        logger.warn "The '#{k}' action name should be a symbol"
-      end
-      if !(v.respond_to?(:call) && v.lambda?)
-        logger.warn "The '#{k}' action should be a lambda function"
-      end
-      if k == :send && v.arity != 2
-        logger.warn "The \'send\' action should take 2 arguments: request and response. #{LEARN_MORE}"
-      end
-      if k != :send && v.arity != 1
-        logger.warn "The '#{k}' action should take 1 argument: request. #{LEARN_MORE}"
-      end
-    end
-    return actions
   end
 end
